e67977445dc3792f116498a3df03e33100b2e282
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2020-08-29 13:08:23 -0600

    Renames brightness to value

--- src/Hsv.hx
+++ src/Hsv.hx
@@ -4,12 +4,12 @@ class Hsv {
 
     public final hue:Float;
     public final saturation:Float;
-    public final brightness:Float;
+    public final value:Float;
 
-    public function new(hue:Float, saturation:Float, brightness:Float) {
+    public function new(hue:Float, saturation:Float, value:Float) {
         this.hue = hue;
         this.saturation = saturation;
-        this.brightness = brightness;
+        this.value = value;
     }
 
 }
