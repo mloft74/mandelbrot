4bb70d4a8194887df9a1617b69f7259f90285dc5
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2020-08-29 13:04:06 -0600

    Adds hsv object

---
+++ src/Hsv.hx
@@ -0,0 +1,15 @@
+package;
+
+class Hsv {
+
+    public final hue:Float;
+    public final saturation:Float;
+    public final brightness:Float;
+
+    public function new(hue:Float, saturation:Float, brightness:Float) {
+        this.hue = hue;
+        this.saturation = saturation;
+        this.brightness = brightness;
+    }
+
+}
